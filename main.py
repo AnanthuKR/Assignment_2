10474e3a32f99c544e3d7f8cfd184d1793eaafa9
Badhusha Muhammed <badhusha.muhammed@pubmaic.com>
2021-10-14 01:34:36 +0530

    assignment comleted commitgit add .

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
+import sys
+import time
+
 from tweepy import StreamListener, OAuthHandler, Stream
 import json
 import argparse
 from Conf import API_TOKEN, API_SECRET, ACCESS_SECRET, ACCESS_TOKEN, MONGO_DB, MONGO_PASSWORD
 from MongoDB import PyMongoConnection
+import datetime
 
 data = []
 pymongoCon = PyMongoConnection(MONGO_PASSWORD, MONGO_DB)
@@ -10,18 +14,26 @@ pymongoCon = PyMongoConnection(MONGO_PASSWORD, MONGO_DB)
 
 def process_data(raw_data):
     tweet_data = json.loads(raw_data)
-    print(tweet_data.get("user").get("favourites_count"))
+    print(" .", end='')
+    sys.stdout.flush()
+    # print(tweet_data['user'])
 
     data.append({"t_id": tweet_data.get("id", "Null"),
+                 "insertedAt": datetime.datetime.now(),
                  "createdAt": tweet_data.get('created_at', "NULL"),
                  "text": tweet_data.get("text", ""),
                  "likes": tweet_data.get("user", {}).get("favourites_count", "Null"),
                  "user_id": tweet_data.get("user", {}).get("id", "Null"),
                  "user_name": tweet_data.get("user", {}).get("screen_name", "Null"),
+                 "profile_url": tweet_data.get("user", {}).get("profile_image_url", "Null")
                  }
                 )
-    if len(data) == 10:
+
+    if len(data) == 100:
+        print("Completed")
+        print("Inserting to MONGODB.............")
         pymongoCon.insert_data(data)
+        print("Completed, Quitting program")
         exit(1)
 
 
@@ -39,23 +51,23 @@ class TwitterStream:
 
     def __init__(self, aut, listener):
         self.stream = Stream(auth=aut, listener=listener)
+        self.batch = 0
 
     def start(self, tags=None):
         if tags is None:
             tags = []
+        print("========================== Streaming Started ============================= \n")
+        print("Streaming .")
         self.stream.filter(track=tags)
-
-    def stop(self):
-        self.stream.disconnect()
 
 
 if __name__ == "__main__":
 
     listener = Listener()
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-topics')
-    args = parser.parse_args()
     aut = OAuthHandler(API_TOKEN, API_SECRET)
+
     aut.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
+
     stream = TwitterStream(aut, listener)
-    stream.start(args.topics.split(","))
+    stream.start(["python", "big data"])
+
